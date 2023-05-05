d662446cb82634ca740de209cc553012df3aa89e
Anastasia Mikheichyk <navstyachka@gmail.com>
2023-05-06 01:28:42 +0200

    feat: email step logic + validation

--- src/components/EmailStep.tsx
+++ src/components/EmailStep.tsx
@@ -3,9 +3,12 @@ import React, { useState, useContext } from 'react'
 import { StepContext } from '../contexts'
 import { PURCHASE_STEPS } from '../constants'
 
+const EMAIL_REGEXP_PATTERN = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
 export const EmailStep = () => {
-  const [email, setEmail] = useState('')
   const { setStepValue, values } = useContext(StepContext)
+
+  const [email, setEmail] = useState('')
+  const [validationError, setValidationError] = useState(false)
 
   return (
     <>
@@ -14,13 +17,26 @@ export const EmailStep = () => {
         <input
           type="email"
           onChange={({ target: { value } }) => {
+            setValidationError(
+              // Only show validation error when user started typing.
+              // Not ideal, but for simplicity this is better than no validation at all.
+              Boolean(value && !EMAIL_REGEXP_PATTERN.test(value))
+            )
             setEmail(value)
           }}
           value={email}
         ></input>
       </div>
 
+      {validationError && (
+        <div>
+          Please check the data you&apos;ve entered, the email should be filled
+          and correct.
+        </div>
+      )}
+
       <button
+        disabled={validationError || !email}
         onClick={() =>
           setStepValue({
             values: {
